9332ee1998e3d800c0418c292e2211a121f415dc
RamosFonseca <beidseitig@gmail.com>
2021-12-31 07:42:42 -0300

    Requisito 6 e ajustes na organizaçao do script, colocando as funçoes e os listeners agrupados

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 const textInput = document.querySelector('#text-input');
-const image = document.querySelector('#meme-image');
-const imageInsert = document.querySelector('#meme-insert')
 const memeText = document.querySelector('#meme-text');
+const image = document.querySelector('#meme-image');
+const imageInsert = document.querySelector('#meme-insert');
+const container = document.querySelector('#meme-image-container');
+const normal = document.querySelector('#normal');
+const fire = document.querySelector('#fire');
+const water = document.querySelector('#water');
+const earth = document.querySelector('#earth');
 
+//Functions
 function addText () {
     memeText.innerHTML = textInput.value
 }
-
-textInput.addEventListener('input', addText);
 
 function addMemeImage () {
     // https://www.webtrickshome.com/forum/how-to-display-uploaded-image-in-html-using-javascript
 	image.src = URL.createObjectURL(event.target.files[0]);
 }
 
+
+function fireStyle () {
+    container.style.border = '3px dashed red';
+}
+
+function waterStyle () {
+    container.style.border = '5px double blue';
+}
+
+function earthStyle () {
+    container.style.border = '6px groove green';
+}
+
+function normalStyle () {
+    container.style.border = '1px solid black';
+}
+
+//Listeners
+textInput.addEventListener('input', addText);
 imageInsert.addEventListener('change', addMemeImage);
+fire.addEventListener('click', fireStyle);
+water.addEventListener('click', waterStyle);
+earth.addEventListener('click', earthStyle);
+normal.addEventListener('click', normalStyle);
